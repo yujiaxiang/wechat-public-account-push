d954469f1fe41b80c9b91e692f3545a9bf05215a
yujiaxiang <75598547+yujiaxiang@users.noreply.github.com>
2022-09-21 22:09:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wxa3d9a66e7af9a4e0",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "530fd58fbd5d2f0b2f0c8cae40df8729",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -62,9 +62,9 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oWGr86n65H1ti2sHCzRbjJIdI46s",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "	C4bONQ6dRoLJ3ORQ9QX-PFC2oWoO98pC7Nt833rxQfE",
       // 所在省份
       province: "重庆",
       // 所在城市

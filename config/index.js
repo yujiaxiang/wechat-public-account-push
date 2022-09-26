c9fcd3e4759899b51169bd217490e8e68cd39a9d
yujiaxiang <75598547+yujiaxiang@users.noreply.github.com>
2022-09-26 14:02:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -139,7 +139,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "	C4bONQ6dRoLJ3ORQ9QX-PFC2oWoO98pC7Nt833rxQfE",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -147,7 +147,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oWGr86n65H1ti2sHCzRbjJIdI46s",
     },
     // 你可以不断按格式往下增加
     // ...
@@ -160,9 +160,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "重庆",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "渝北",
 
   /** 重要节日相关 */
 
